9a51a055a53856ed998e2d4ebebb03dd3badbf90
Raman Hira <ramanhira@gmail.com>
2022-10-03 00:45:38 +0530

    added the exercise for Day 19

---
+++ Day19.js
@@ -0,0 +1,13 @@
+/**
+ * What is the output and why ?
+ */
+
+var salary = '1000$';
+
+(function () {
+    console.log(`Original Salary was: ${salary}`);
+
+    var salary = '5000%';
+
+    console.log(`Now the salary is: ${salary}`);
+})();
